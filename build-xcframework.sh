3ead7c967303fb3b69c7a24d694d1ff6fec19a2e
Andrii Pervushyn <andrii@icat.solutions>
2024-02-27 17:11:13 +0200

    Min ios 15

--- build-xcframework.sh
+++ build-xcframework.sh
@@ -1,9 +1,9 @@
-#xcodebuild clean archive -project FlagPhoneNumber.xcodeproj -scheme "FlagPhoneNumber" -sdk iphoneos OBJROOT=build/iOS
-#xcodebuild clean archive -project FlagPhoneNumber.xcodeproj -scheme "FlagPhoneNumber" -sdk iphonesimulator OBJROOT=build/simulator
-#xcodebuild -create-xcframework \
-#  -framework build/iOS/UninstalledProducts/iphoneos/FlagPhoneNumber.framework \
-#  -framework build/simulator/UninstalledProducts/iphonesimulator/FlagPhoneNumber.framework \
-#  -output build/FlagPhoneNumber.xcframework
+xcodebuild clean archive -project FlagPhoneNumber.xcodeproj -scheme "FlagPhoneNumber" -sdk iphoneos OBJROOT=build/iOS
+xcodebuild clean archive -project FlagPhoneNumber.xcodeproj -scheme "FlagPhoneNumber" -sdk iphonesimulator OBJROOT=build/simulator
+xcodebuild -create-xcframework \
+  -framework build/iOS/UninstalledProducts/iphoneos/FlagPhoneNumber.framework \
+  -framework build/simulator/UninstalledProducts/iphonesimulator/FlagPhoneNumber.framework \
+  -output build/FlagPhoneNumber.xcframework
 
   # Create the ZIP file
 zip -r -X build/FlagPhoneNumber.xcframework.zip build/FlagPhoneNumber.xcframework
